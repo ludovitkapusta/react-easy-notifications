345af88d4c4df270dcdcff1f4613d5c0a4d10c07
escuriak <ludovit.kapusta@gmail.com>
2018-02-18 13:21:50 +0100

    new bundle

--- build/index.js
+++ build/index.js
@@ -670,6 +670,14 @@ var Notification = function (_React$Component) {
     return Notification;
 }(_react2.default.Component);
 
+Notification.PropTypes = {
+    title: _propTypes2.default.string,
+    content: _propTypes2.default.string,
+    duration: _propTypes2.default.number,
+    onCreate: _propTypes2.default.func,
+    onClick: _propTypes2.default.func
+};
+
 exports.default = Notification;
 
 /***/ }),
@@ -711,70 +719,6 @@ Object.defineProperty(exports, "__esModule", {
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 exports.default = {
-    /**
-     * @method toArray
-     * @description Converts non-array variable to array.
-     * @param {Any} variable
-     * @return {Array}
-     */
-    toArray: function toArray(variable) {
-        return Array.isArray(variable) ? variable : [variable];
-    },
-
-
-    /**
-     * @method includes
-     * @description Checks if provided array includes value/values.
-     * @param {Array} array Source array.
-     * @param {Array|Any} values Any kind of values (string, number, array).
-     * @param {Boolean} strict Comparison strategy. When {true}, should include all values.
-     * @return {Boolean}
-     */
-    includes: function includes(array, values) {
-        var strict = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-
-        var valuesCollection = Array.isArray(values) ? values : [values];
-        var includes = false;
-
-        /* Use "for" loop to stop iteration once resolved {true} in non-strict mode */
-        for (var i = 0; i < valuesCollection.length; i++) {
-            var expectedValue = valuesCollection[i];
-
-            if (array.includes(expectedValue)) {
-                includes = true;
-
-                if (!strict) {
-                    break;
-                }
-            } else if (strict) {
-                includes = false;
-            }
-        }
-
-        return includes;
-    },
-
-
-    /**
-     * @method fromNodes
-     * @description Converts array-like Object(s) to array.
-     * @param {HTMLCollection|NodeList|DOMElement} nodes
-     * @return {Array} Array of the provided nodes.
-     */
-    fromNodes: function fromNodes(nodes) {
-        if (Array.isArray(nodes)) {
-            return nodes;
-        }
-        return nodes instanceof HTMLCollection || nodes instanceof NodeList ? Array.from(nodes) : [nodes];
-    },
-
-
-    /**
-     * @method merge
-     * @description Shallow merge set of String/Array into Array and unqiue.
-     * @param {Array} arrays
-     * @return {Array}
-     */
     merge: function merge() {
         var merged = [];
 
@@ -794,72 +738,17 @@ exports.default = {
 
         return merged;
     },
-
-
-    /**
-     * @method join
-     * @description Joins array values using certain delimiter.
-     * @param {Array} arrays
-     * @return {Array}
-     */
     join: function join() {
         return this.merge.apply(this, arguments).join(' ');
     },
 
 
-    /**
-     * @method unique
-     * @description Remove duplicated from the Array entries.
-     * @param {Array} array Source array.
-     * @return {Array} Reference to the modified array.
-     */
     unique: function unique(array) {
         return Array.from(new Set(array));
     },
 
-    /**
-     * @method trim
-     * @description Removes empty values from Array.
-     * @param {Array} array
-     * @return {Array}
-     */
     trim: function trim(array) {
         return array.filter(Boolean);
-    },
-
-
-    /* Find and remove item from an array */
-    /**
-     * @method trim
-     * @description Find and remove item from an array.
-     * @param {Array} array Source array.
-     * @param {Any} item Item to remove.
-     * @return {Array}
-     */
-    remove: function remove(array, item) {
-        var i = array.indexOf(item);
-        if (i !== -1) {
-            array.splice(i, 1);
-        }
-
-        return array;
-    },
-
-
-    /**
-     * @method flatten
-     * @description Flattens passed array into one-level array.
-     * @param {Array} array
-     * @return {Array}
-     */
-    flatten: function flatten(array) {
-        if (Array.isArray(array)) {
-            return array.reduce(function (done, current) {
-                return done.concat(current);
-            }, []);
-        }
-
-        return array;
     }
 };
 
